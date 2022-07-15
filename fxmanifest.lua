30cc2279d24b4bf3249a8476232b72aea56f2a29
sfxcustoms_originals <77953089+sfxcustoms@users.noreply.github.com>
2022-07-15 15:46:11 +0800

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 		'cerulean'
+use_fxv2_oal    'yes'
+lua54           'yes'
+game 			'gta5'
+
+version 		'ver:1.0-sfxcustoms'
+author 			'Saitama#5230'
+description 	'SFX Customs Originals | Discord: https://discord.gg/5ZrJBrKwpG'
+
+client_scripts {
+    'client/main.lua',
+}
+
+server_scripts {
+    'server/main.lua',
+}
